8841e6542ca3251fea57b9c05d5e4212b1058511
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-08-18 15:12:16 +0300

    Completed assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    numbers = [n for n in num_list if n % 2 == 0]
+    return numbers
+  
+return_evens([0,1,3,5,7,8,9])              
 
 def make_exclamation(sentence_list):
-    pass
+    sentence = [x + '!' for x in sentence_list ]
+    return sentence
+make_exclamation(['Test', 'Sentence'])
+
